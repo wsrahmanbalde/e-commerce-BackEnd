2e9cf4772c29aba7e47771fbfd2f9d1daabd7a00
Balde <abdourahamane620@gmail.com>
2024-06-09 05:09:24 +0200

    Ajout de la methode add du produit

--- src/main/java/com/commerce/Services/Produit/ProduitServiceImpl.java
+++ src/main/java/com/commerce/Services/Produit/ProduitServiceImpl.java
@@ -1,12 +1,20 @@
 package com.commerce.Services.Produit;
 
 import com.commerce.Models.Produit;
+import com.commerce.Repository.ProduitRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProduitServiceImpl  implements ProduitService{
+
+    private final ProduitRepository produitRepository;
+
+    public ProduitServiceImpl(ProduitRepository produitRepository) {
+        this.produitRepository = produitRepository;
+    }
+
     @Override
     public Produit addProduit(Produit produit) {
-        return null;
+        return produitRepository.save(produit);
     }
 }
