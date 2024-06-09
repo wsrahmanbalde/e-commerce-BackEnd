a0289be562412229f5a425ea7ee126a95023cec7
Balde <abdourahamane620@gmail.com>
2024-06-09 05:36:39 +0200

    Test

--- src/test/java/com/commerce/CommerceApplicationTests.java
+++ src/test/java/com/commerce/CommerceApplicationTests.java
@@ -1,13 +1,22 @@
 package com.commerce;
 
+import com.commerce.Models.Produit;
+import com.commerce.Repository.ProduitRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 class CommerceApplicationTests {
 
+	@Autowired
+	private ProduitRepository repository;
 	@Test
 	void contextLoads() {
+		repository.save( new Produit(1,"nom",12D,new Date())
+		);
 	}
 
 }
