d27cd1bb34a755b512011098be09d45e58d197a2
Balde <abdourahamane620@gmail.com>
2024-06-09 05:17:56 +0200

    Modifiaction

--- src/main/java/com/commerce/Models/Produit.java
+++ src/main/java/com/commerce/Models/Produit.java
@@ -1,6 +1,9 @@
 package com.commerce.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 import java.util.Date;
@@ -12,6 +15,9 @@ import java.util.Date;
 @Builder
 @Entity
 public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nom;
     private Double prix;
