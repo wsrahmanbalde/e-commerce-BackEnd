f6bfdc69b9931c69a1b4e2b2c71fba08aef9703e
Balde <abdourahamane620@gmail.com>
2024-06-09 05:03:08 +0200

    Creation du model produit

--- src/main/java/com/commerce/Models/Produit.java
+++ src/main/java/com/commerce/Models/Produit.java
@@ -1,4 +1,19 @@
 package com.commerce.Models;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
 public class Produit {
+    private int id;
+    private String nom;
+    private Double prix;
+    private Date dateCreation;
 }
