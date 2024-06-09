2a3597bc739fcded6bb307a8bb7c9ef2bf4067a4
Balde <abdourahamane620@gmail.com>
2024-06-09 05:08:03 +0200

    Creation des services produit

---
+++ src/main/java/com/commerce/Services/Produit/ProduitService.java
@@ -0,0 +1,7 @@
+package com.commerce.Services.Produit;
+
+import com.commerce.Models.Produit;
+
+public interface ProduitService {
+    Produit addProduit(Produit produit);
+}
---
+++ src/main/java/com/commerce/Services/Produit/ProduitServiceImpl.java
@@ -0,0 +1,12 @@
+package com.commerce.Services.Produit;
+
+import com.commerce.Models.Produit;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProduitServiceImpl  implements ProduitService{
+    @Override
+    public Produit addProduit(Produit produit) {
+        return null;
+    }
+}
