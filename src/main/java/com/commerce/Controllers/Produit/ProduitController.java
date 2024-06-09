5cbf14781a64496e41366c62f37cc1cffa25c323
Balde <abdourahamane620@gmail.com>
2024-06-09 05:13:21 +0200

    Ajout du controller add du produit

---
+++ src/main/java/com/commerce/Controllers/Produit/ProduitController.java
@@ -0,0 +1,24 @@
+package com.commerce.Controllers.Produit;
+
+import com.commerce.Models.Produit;
+import com.commerce.Services.Produit.ProduitService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/produits")
+@RestController
+public class ProduitController {
+
+    private final ProduitService produitService;
+
+
+    public ProduitController(ProduitService produitService) {
+        this.produitService = produitService;
+    }
+
+    @PostMapping()
+    Produit createProduit(Produit produit){
+        return this.produitService.addProduit(produit);
+    }
+}
