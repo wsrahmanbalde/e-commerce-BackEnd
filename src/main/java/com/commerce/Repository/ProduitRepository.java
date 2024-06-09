4fa71c652037c96971bc806b684017108e02ed53
Balde <abdourahamane620@gmail.com>
2024-06-09 18:24:36 +0200

    JPQL

--- src/main/java/com/commerce/Repository/ProduitRepository.java
+++ src/main/java/com/commerce/Repository/ProduitRepository.java
@@ -4,6 +4,11 @@ import com.commerce.Models.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProduitRepository extends JpaRepository<Produit,Long> {
+
+    List<Produit> findByNom(String nom);
+    List<Produit> findByNomContains(String nom);
 }
